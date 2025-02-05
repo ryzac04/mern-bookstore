1c9ecff1d8077cb32b16de57a35150b1ea8c8364
ryzac04 <rzc044@gmail.com>
2025-02-05 08:24:36 -0600

    Set up Home component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default App
